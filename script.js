1b9dcdb97eabc7bb0267b85165a30c27ceac86d1
KristianaTR <82210631+KristianaTR@users.noreply.github.com>
2022-04-12 09:50:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+function getData(){
+const rawData = document.querySelector("#data_source").value;
+const refinedData = rawData.split();
+
+console.log(refinedData);
+//return refinedData;
+};
+
+
+
+function getStreetNames(){
+
+};
