48b72387b3c95333198a7d4a939dd98d854d136f
KristianaTR <82210631+KristianaTR@users.noreply.github.com>
2022-04-26 10:23:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,48 @@
 
 function getData(){
 const rawData = document.querySelector("#data_source").value;
-const refinedData = rawData.split();
+const filteredDataFirst = rawData.split("<table")[1];
 
-console.log(refinedData);
+const filteredDataSecond = filteredDataFirst.split("</table>")[0];
+
+const filteredTableRows = filteredDataSecond.split("</tr>");
+//console.log(filteredTableRows);
+
+
+/* Izcēlu ārā divus mainīgos, lai tos varētu izmantot vairāki, viens otram sekojoši cikli 
+-> BET ŠIS NENOSTRĀDĀJA  */
+let filteredSteetCells = []; 
+let streetName = [];
+
+for(let i = 1; i <= filteredTableRows.length; i++){
+    filteredSteetCells[i-1] = filteredTableRows[i].split("</td>")[0];
+   streetName[i-1] = filteredSteetCells[i-1].split('">')[1];
+   console.log(streetName[i-1].split("</a>")[0]);
+   return (streetName[i-1].split("</a>")[0]);
+};
+/*
+for (let j = 0; j <= filteredSteetCells.length; j++){
+    console.log(filteredSteetCells[j].split('">')[1]);
+};
+*/
+/*for(let j = 1; j<= filteredData.lenght; j++){
+ console.log(filteredData[j].split(""))
+}*/
+
+//console.log(filteredTableRows);
 //return refinedData;
+
+
 };
 
 
 
 function getStreetNames(){
-
+    var streetNames = getData();
+    const resultContainer = document.querySelector(".street_names");
+    const streetContainer = document.createElement("div");
+    streetContainer.className = "street_container";
+    streetContainer.innerHTML = streetNames;
+    resultContainer.append(streetContainer);
+    
 };
